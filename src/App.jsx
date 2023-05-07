e7673f85b4b9f703688d2efad982899aa9d15dc4
ajea <smithajea@rocketmail.com>
2023-05-07 08:11:18 -0400

    worked on navbar menu for mobile and tablet sizes

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,131 @@ function App() {
 	return (
 		<>
 			<Header />
-			{/* <div className=''>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+			{/* <h1 className="H1">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+				dignissimos temporibus harum, natus quas rem maxime quis esse
+				quia numquam totam ipsum eius optio sed assumenda amet et in
+				explicabo doloremque. Libero inventore accusantium ipsam?
+				Facilis laboriosam aliquam commodi neque optio accusamus nulla,
+				aut veritatis quaerat labore magnam vel tempore recusandae
+				corporis odio doloremque sequi quisquam delectus enim
+				exercitationem adipisci rerum sapiente. Modi pariatur dolorum
+				necessitatibus autem maiores. Sapiente dolor doloribus tenetur
+				sequi adipisci quis est eligendi deserunt nostrum, veritatis
+				officiis debitis at enim maiores laborum, quisquam ab totam
+				nesciunt ratione. Iusto nesciunt in mollitia debitis, explicabo
+				omnis fugit natus blanditiis incidunt maxime nihil aut tempora
+				cupiditate facere eligendi amet animi quidem voluptatum! Maiores
+				accusamus enim, corporis dignissimos optio asperiores saepe
+				eveniet velit itaque iusto consectetur fugit laudantium totam.
+				Omnis at perferendis ab nostrum officia, autem ratione illum
+				quisquam. Voluptatibus iste nihil expedita veritatis voluptate.
+				Ipsum ab, repellendus repellat, at nisi similique, eveniet
+				dolorum esse aut voluptatibus atque tenetur. Natus quia
+				assumenda, esse sunt corrupti est dicta eligendi ipsum eaque
+				cupiditate dolorum consequatur voluptate atque labore facere
+				beatae blanditiis, velit sequi modi non placeat. Eos distinctio
+				aliquid vel quam? Assumenda labore autem, illum placeat culpa
+				sit neque perspiciatis expedita ea qui, quisquam quis deleniti.
+				Odio ea similique rem tenetur, dolor officia neque doloribus
+				temporibus dolores. Blanditiis quo sit, error eveniet
+				consequatur dolorum quas voluptas inventore dolore cumque
+				laborum voluptatum nesciunt obcaecati deserunt in quisquam
+				maxime? Ab, sed saepe illo aliquam nesciunt consequuntur
+				possimus necessitatibus nisi voluptates. Fugit id architecto vel
+				nulla illo tempore et, rem facilis sunt officia eligendi
+				aliquid, voluptatibus iure ut vero quis illum accusamus. Commodi
+				voluptas rem vitae atque libero dicta perspiciatis dolorem nisi
+				a cupiditate hic aliquid provident, delectus temporibus eius
+				maxime autem tempora soluta sequi minus dolorum quis, esse
+				itaque harum. Vero, enim. Possimus deleniti rem asperiores unde
+				quam ex sapiente error adipisci a facilis excepturi, ut dolores
+				enim officia, quis nulla odio id ducimus magni fuga officiis
+				mollitia? Dicta asperiores sed distinctio. Quidem aperiam
+				sapiente porro sequi, explicabo voluptas. Voluptas deleniti
+				repellat quod inventore ullam rem molestias quam illum culpa
+				aut. Illum molestias non, aliquid sequi ipsam in temporibus cum
+				voluptatum corporis, architecto veniam modi mollitia soluta
+				animi pariatur ut natus ipsa reprehenderit accusamus suscipit
+				atque eius eum. Atque blanditiis laboriosam magni sint!
+				Assumenda itaque id odio cumque, aliquid aspernatur consequuntur
+				iste praesentium veritatis culpa totam et laborum ea. Corrupti
+				nulla saepe, commodi officiis voluptatum dolor mollitia
+				perspiciatis nisi voluptate ipsum incidunt nesciunt, quia
+				dolore, fugiat debitis eligendi consequatur temporibus. Totam
+				consequuntur quod voluptate quis minima tempora rem corporis
+				voluptatem, incidunt, deleniti eius iusto ex et perferendis
+				aspernatur laborum quisquam quia voluptatum laboriosam, pariatur
+				quidem ullam! Voluptates molestiae voluptas atque architecto ex
+				soluta consequatur quo est perferendis libero neque aliquam
+				ipsam, doloremque sunt odio enim, dicta maiores rerum accusamus
+				minus. Aliquam vero natus ex veniam, maxime possimus sit facilis
+				repellendus atque unde suscipit minima nam optio libero quia et
+				vel. Harum, fugit perferendis, officia autem cum commodi quis
+				ratione nesciunt aspernatur quidem nobis pariatur saepe
+				cupiditate libero, debitis nihil? Illo nulla labore doloribus
+				eveniet quibusdam facere velit totam laborum, autem nemo
+				corrupti, fugiat tempore error quisquam, repudiandae magni iusto
+				fuga. Blanditiis quidem ad dolorum impedit sit accusantium velit
+				soluta itaque dolores in, pariatur ratione voluptas voluptatem,
+				eum, error accusamus aperiam nemo laudantium? Neque debitis
+				voluptas soluta culpa repellat. Totam debitis eum eaque at
+				itaque hic tempore sed sint, quia voluptates rerum, non quod
+				voluptatibus ab fuga perspiciatis commodi! Ad, velit corporis
+				sunt itaque pariatur dolores necessitatibus nulla labore in
+				quae, sapiente, excepturi ipsum magnam esse nobis! Harum quo
+				nesciunt soluta aliquam? Inventore assumenda accusamus, commodi
+				temporibus in dignissimos et quibusdam eaque, corrupti
+				architecto, vitae omnis ut cumque voluptate ducimus incidunt
+				officiis quasi illum porro consectetur esse itaque aut
+				perferendis nobis. Perspiciatis provident fugit corrupti ducimus
+				recusandae quos, consequuntur, aliquid iure consectetur voluptas
+				rerum quas dolores sapiente suscipit error quidem nemo dolorum.
+				Doloribus laboriosam alias error necessitatibus rerum cupiditate
+				asperiores odio, architecto labore illum, temporibus
+				consequuntur iusto accusamus, eaque ad esse reiciendis deleniti
+				sit eum voluptas! Officiis, consectetur dolor repellat
+				reprehenderit quidem eaque rem mollitia modi possimus laborum
+				ducimus nemo inventore qui ab rerum voluptate cumque aut
+				similique velit quae. Illo veritatis voluptate quibusdam
+				corporis mollitia. Obcaecati eius necessitatibus esse iusto
+				fugiat! Perspiciatis odit placeat ea repellendus alias ex
+				incidunt veritatis vel, sed aperiam, ab quia temporibus velit
+				nulla illum ratione corporis. Voluptatem officiis expedita quae
+				impedit? Repellat soluta quisquam illo nostrum accusantium, iste
+				at reprehenderit sapiente id impedit consectetur debitis totam
+				nihil ipsum quas possimus vel quidem aspernatur explicabo unde
+				nam. Cupiditate vero delectus eius sequi non facilis mollitia
+				nemo molestias amet omnis quasi molestiae vitae aut blanditiis
+				asperiores officiis qui nulla atque, commodi velit excepturi
+				maiores quae. Obcaecati molestias ducimus minima repudiandae
+				assumenda nostrum suscipit voluptate, animi dignissimos
+				explicabo officiis mollitia minus cum a, illum optio maiores
+				dolor iure neque aliquid placeat enim. Doloremque doloribus,
+				explicabo consequatur, perferendis quidem tempora tempore
+				eligendi eaque illo veniam voluptatibus, omnis non nobis
+				repellat quis aliquid iure similique architecto? Fuga quia
+				laborum alias, rem accusamus unde recusandae earum libero quidem
+				neque voluptate corrupti, assumenda qui, quibusdam eum sunt
+				animi! Eum, debitis. Repellendus ab inventore veniam quis nobis!
+				Harum, dignissimos. Voluptatem tempore, ut vel quibusdam error
+				necessitatibus qui nam sunt. Similique, saepe culpa.
+				Necessitatibus iusto quisquam harum, dolorum iure blanditiis
+				temporibus eaque perferendis beatae veritatis repudiandae natus
+				ducimus nemo officia. Assumenda, consequuntur, magni sed dolor
+				repellat expedita inventore eum alias ut amet laboriosam quaerat
+				laborum, fugit est totam qui nihil. Ea quae omnis distinctio
+				maxime voluptatem enim nisi recusandae earum a numquam dicta,
+				obcaecati doloribus, accusamus nobis similique at tempora id
+				vero ut eaque deserunt? Ex voluptates cumque fugiat quis aliquam
+				dolorum molestias, at quos dolore tenetur omnis iste corrupti
+				expedita error facere quisquam pariatur, eum hic fuga. At natus
+				animi ratione maxime nihil ab quod veritatis exercitationem
+				ipsum asperiores dolore beatae, iure totam ad atque dolores
+				placeat rem repellendus nam possimus, vero quis harum ut cum.
+				Exercitationem praesentium, autem accusantium corporis
+				reprehenderit odit harum tempore dignissimos! Esse?
+			</h1> */}
 		</>
 	);
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import iconHamburger from "../assets/icon-hamburger.svg";
 import iconChevron from "../assets/icon-chevron.svg";
 import data from "../data.json";
 const Header = () => {
@@ -9,50 +8,69 @@ const Header = () => {
 		setIsMenuOpen(!isMenuOpen);
 	};
 	return (
-		<header className="px-6 py-4">
-			<div>
-				<div className="flex justify-between">
-					<p>The Planets</p>
+		<header className="flex flex-col md:items-center md:border-b md:border-b-slate-800">
+			<div className="border-b border-b-slate-800 md:mr-5 md:border-none">
+				<div className="flex items-center justify-between px-6 pt-4 pb-[17px] w-full">
+					<p className="text-[28px] uppercase font-normal font-['Antonio']">
+						The Planets
+					</p>
 					<button
-						className="text-lg font-bold text-gray-600 focus:outline-none"
 						onClick={toggleMenu}
+						className="md:hidden"
+						aria-controls="menu"
+						aria-expanded="false"
 					>
-						<img src={iconHamburger} alt="" />
+						<span className="sr-only">Menu</span>
+						<svg
+							opacity={isMenuOpen ? 0.4 : 1}
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="17"
+						>
+							<g fill="#FFF" fillRule="evenodd">
+								<path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" />
+							</g>
+						</svg>
 					</button>
 				</div>
-				<nav
-					className="flex items-center justify-between py-2 bg-gray-100"
-					aria-label="Site Navigation"
-				>
-					<ul
-						className={`flex flex-col w-full ${
-							isMenuOpen ? "block" : "hidden"
-						}`}
-					>
-						{data.map((planet) => {
-							return (
-								<li
-									key={planet.name}
-									className="menu-item flex justify-between font-bold text-[15px] uppercase tracking-[1.36px] py-5 grow basis-0 border-b border-slate-800"
-								>
-									<div className="flex">
-										<div
-											className={`w-5 h-5 ${planet.name}-color rounded-full mr-6`}
-										></div>
-										<a className="block" href="#">
-											{planet.name}
-										</a>
-									</div>
-									<img
-										src={iconChevron}
-										alt="right chevron"
-									/>
-								</li>
-							);
-						})}
-					</ul>
-				</nav>
 			</div>
+			<nav
+				className="bg-blueBlack flex items-center px-6 mt-[22px] md:w-full md:justify-between md:mt-0 md:px-0"
+				aria-label="Site Navigation"
+			>
+				<ul
+					className={`w-full ${
+						isMenuOpen ? "flex flex-col" : "hidden"
+					} md:flex md:flex-row md:justify-evenly`}
+					role="list"
+				>
+					{data.map((planet) => {
+						return (
+							<li
+								key={planet.name}
+								className="menu-item flex justify-between font-bold text-[15px] uppercase tracking-[1.36px] py-5 grow basis-0 border-b border-slate-800 md:grow-0 md:border-b-0"
+							>
+								<div className="flex">
+									<div
+										className={`w-5 h-5 ${planet.name}-color rounded-full mr-6 md:hidden`}
+									></div>
+									<a
+										className="block md:inline-block"
+										href="#"
+									>
+										{planet.name}
+									</a>
+								</div>
+								<img
+									className="md:hidden"
+									src={iconChevron}
+									alt="right chevron"
+								/>
+							</li>
+						);
+					})}
+				</ul>
+			</nav>
 		</header>
 	);
 };
